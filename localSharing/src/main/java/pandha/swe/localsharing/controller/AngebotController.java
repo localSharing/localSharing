3c8b644c8f4e5bba23f80008fa4ff5b2996f1bdc
Julia Anken <jul.anken@googlemail.com>
2014-12-01 11:56:22 +0100

    richtige angebote anzeigen

--- localSharing/src/main/java/pandha/swe/localsharing/controller/AngebotController.java
+++ localSharing/src/main/java/pandha/swe/localsharing/controller/AngebotController.java
@@ -1,8 +1,6 @@
 package pandha.swe.localsharing.controller;
 
 import java.security.Principal;
-import java.sql.Date;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import pandha.swe.localsharing.model.Ausleihartikel;
 import pandha.swe.localsharing.model.Benutzer;
 import pandha.swe.localsharing.model.dto.AusleihartikelDTO;
 import pandha.swe.localsharing.model.dto.HilfeleistungDTO;
@@ -42,76 +41,19 @@ public class AngebotController {
 
 		Benutzer user = getUser(principal);
 
-		// Beispieldaten zum Anzeigen
-
-		AusleihartikelDTO ausleihartikel1 = new AusleihartikelDTO(new Long(1),
-				user, "Tarzan", "Tolle DVD", Date.valueOf("2014-11-27"),
-				Date.valueOf("2014-12-27"), 3, "DVD");
-
-		AusleihartikelDTO ausleihartikel2 = new AusleihartikelDTO(new Long(2),
-				user, "Mickey Mouse", "tolles Buch",
-				Date.valueOf("2014-11-27"), Date.valueOf("2014-12-27"), 3,
-				"Buch");
-
-		AusleihartikelDTO ausleihartikel3 = new AusleihartikelDTO(new Long(3),
-				user, "Simpsons", "Tolle DVD", Date.valueOf("2014-11-27"),
-				Date.valueOf("2014-12-27"), 3, "DVD");
-
-		TauschartikelDTO tauschartikel1 = new TauschartikelDTO(new Long(1),
-				user, "Tarzan", "Tolle DVD", Date.valueOf("2014-11-27"), "DVD");
-
-		TauschartikelDTO tauschartikel2 = new TauschartikelDTO(new Long(2),
-				user, "Mickey Mouse", "tolles Buch",
-				Date.valueOf("2014-11-27"), "Buch");
-
-		TauschartikelDTO tauschartikel3 = new TauschartikelDTO(new Long(3),
-				user, "Simpsons", "Tolle DVD", Date.valueOf("2014-11-27"),
-				"DVD");
-
-		HilfeleistungDTO hilfe1 = new HilfeleistungDTO(new Long(1), user,
-				"Streichen", "Ich kann das", Date.valueOf("2014-11-23"),
-				Date.valueOf("2014-12-11"));
-
-		HilfeleistungDTO hilfe2 = new HilfeleistungDTO(new Long(2), user,
-				"Rasen mähen", "Ich bin super", Date.valueOf("2014-11-23"),
-				Date.valueOf("2014-12-11"));
-
-		HilfeleistungDTO hilfe3 = new HilfeleistungDTO(new Long(3), user,
-				"Einkaufen", "Ich will eben", Date.valueOf("2014-11-23"),
-				Date.valueOf("2014-12-11"));
-
 		// Liste mit allen Ausleihangeboten eines Benutzers
-		// List<AusleihartikelDTO> aArtikel = ausleihartikelService
-		// .findAllByBenutzer(user);
+		List<AusleihartikelDTO> aArtikel = ausleihartikelService
+				.findAllByBenutzer(user);
 
 		// Liste mit allen Tauschangeboten eines Benutzers
-		// List<TauschartikelDTO> tArtikel = tauschartikelService
-		// .findAllByBenutzer(user);
+		List<TauschartikelDTO> tArtikel = tauschartikelService
+				.findAllByBenutzer(user);
 
 		// Liste mit allen Hilfeleistungen eines Benutzers
-		// List<TauschartikelDTO> hArtikel = hilfeleistungService
-		// .findAllByBenutzer(user);
+		List<HilfeleistungDTO> hArtikel = hilfeleistungService
+				.findAllByBenutzer(user);
 
 		// Liste Model hinzufügen
-		// model.addAttribute("artikelListA", aArtikel);
-		// model.addAttribute("artikelListT", tArtikel);
-		// model.addAttribute("artikelListH", hArtikel);
-
-		List<AusleihartikelDTO> aArtikel = new ArrayList<AusleihartikelDTO>();
-		aArtikel.add(ausleihartikel1);
-		aArtikel.add(ausleihartikel2);
-		aArtikel.add(ausleihartikel3);
-
-		List<TauschartikelDTO> tArtikel = new ArrayList<TauschartikelDTO>();
-		tArtikel.add(tauschartikel1);
-		tArtikel.add(tauschartikel2);
-		tArtikel.add(tauschartikel3);
-
-		List<HilfeleistungDTO> hArtikel = new ArrayList<HilfeleistungDTO>();
-		hArtikel.add(hilfe1);
-		hArtikel.add(hilfe2);
-		hArtikel.add(hilfe3);
-
 		model.addAttribute("artikelListA", aArtikel);
 		model.addAttribute("artikelListT", tArtikel);
 		model.addAttribute("artikelListH", hArtikel);
@@ -123,18 +65,13 @@ public class AngebotController {
 	public String showAngebot(Model model, Principal principal,
 			@PathVariable("ID") String id, @PathVariable("Type") String type) {
 
-		Benutzer user = getUser(principal);
 		switch (type) {
 		case "ausleihen":
-			// AusleihartikelDTO ausleihartikel = ausleihartikelService
-			// .ausleihartikel_TO_AusleihartikelDTO(ausleihartikelService
-			// .findById(new Long(id)));
+			AusleihartikelDTO ausleihartikel = ausleihartikelService
+					.ausleihartikel_TO_AusleihartikelDTO(ausleihartikelService
+							.findById(new Long(id)));
 
-			// Daten zum Anzeigen in der View
-			AusleihartikelDTO ausleihartikel = new AusleihartikelDTO(new Long(
-					id), user, "Tarzan", "Tolle DVD",
-					Date.valueOf("2014-11-27"), Date.valueOf("2014-12-27"), 3,
-					"DVD");
+			System.out.println(ausleihartikel);
 
 			model.addAttribute("angebot", ausleihartikel);
 			model.addAttribute("endDatum", ausleihartikel.getEndDatum());
@@ -143,14 +80,9 @@ public class AngebotController {
 			break;
 
 		case "tauschen":
-			// TauschartikelDTO tauschartikel = tauschartikelService
-			// .tauschartikel_TO_TauschartikelDTO(tauschartikelService
-			// .findById(new Long(id)));
-
-			// Daten zum Anzeigen
-			TauschartikelDTO tauschartikel = new TauschartikelDTO(new Long(id),
-					user, "Mickey Mouse", "tolles Buch",
-					Date.valueOf("2014-11-27"), "Buch");
+			TauschartikelDTO tauschartikel = tauschartikelService
+					.tauschartikel_TO_TauschartikelDTO(tauschartikelService
+							.findById(new Long(id)));
 
 			model.addAttribute("angebot", tauschartikel);
 			model.addAttribute("kategorie", tauschartikel.getKategorie());
@@ -158,14 +90,9 @@ public class AngebotController {
 			break;
 
 		case "helfen":
-			// HilfeleistungDTO hilfeleistung = hilfeleistungService
-			// .hilfeleistung_TO_HilfeleistungDTO(hilfeleistungService
-			// .findById(new Long(id)));
-
-			// Daten zum Anzeigen
-			HilfeleistungDTO hilfeleistung = new HilfeleistungDTO(new Long(id),
-					user, "Einkaufen", "Ich will eben",
-					Date.valueOf("2014-11-23"), Date.valueOf("2014-12-11"));
+			HilfeleistungDTO hilfeleistung = hilfeleistungService
+					.hilfeleistung_TO_HilfeleistungDTO(hilfeleistungService
+							.findById(new Long(id)));
 
 			model.addAttribute("angebot", hilfeleistung);
 			model.addAttribute("endDatum", hilfeleistung.getEndDatum());
@@ -180,19 +107,11 @@ public class AngebotController {
 	public String editAngebot(Model model, Principal principal,
 			@PathVariable("id") String id, @PathVariable("type") String type) {
 
-		Benutzer user = getUser(principal);
-
 		switch (type) {
 		case "ausleihen":
-			// AusleihartikelDTO ausleihartikel = ausleihartikelService
-			// .ausleihartikel_TO_AusleihartikelDTO(ausleihartikelService
-			// .findById(new Long(id)));
-
-			// Daten zum Anzeigen in der View
-			AusleihartikelDTO ausleihartikel = new AusleihartikelDTO(new Long(
-					id), user, "Tarzan", "Tolle DVD",
-					Date.valueOf("2014-11-27"), Date.valueOf("2014-12-27"), 3,
-					"DVD");
+			AusleihartikelDTO ausleihartikel = ausleihartikelService
+					.ausleihartikel_TO_AusleihartikelDTO(ausleihartikelService
+							.findById(new Long(id)));
 
 			model.addAttribute("angebot", ausleihartikel);
 			model.addAttribute("endDatum", ausleihartikel.getEndDatum());
@@ -201,14 +120,9 @@ public class AngebotController {
 			break;
 
 		case "tauschen":
-			// TauschartikelDTO tauschartikel = tauschartikelService
-			// .tauschartikel_TO_TauschartikelDTO(tauschartikelService
-			// .findById(new Long(id)));
-
-			// Daten zum Anzeigen
-			TauschartikelDTO tauschartikel = new TauschartikelDTO(new Long(id),
-					user, "Mickey Mouse", "tolles Buch",
-					Date.valueOf("2014-11-27"), "Buch");
+			TauschartikelDTO tauschartikel = tauschartikelService
+					.tauschartikel_TO_TauschartikelDTO(tauschartikelService
+							.findById(new Long(id)));
 
 			model.addAttribute("angebot", tauschartikel);
 			model.addAttribute("kategorie", tauschartikel.getKategorie());
@@ -216,14 +130,9 @@ public class AngebotController {
 			break;
 
 		case "helfen":
-			// HilfeleistungDTO hilfeleistung = hilfeleistungService
-			// .hilfeleistung_TO_HilfeleistungDTO(hilfeleistungService
-			// .findById(new Long(id)));
-
-			// Daten zum Anzeigen
-			HilfeleistungDTO hilfeleistung = new HilfeleistungDTO(new Long(id),
-					user, "Einkaufen", "Ich will eben",
-					Date.valueOf("2014-11-23"), Date.valueOf("2014-12-11"));
+			HilfeleistungDTO hilfeleistung = hilfeleistungService
+					.hilfeleistung_TO_HilfeleistungDTO(hilfeleistungService
+							.findById(new Long(id)));
 
 			model.addAttribute("angebot", hilfeleistung);
 			model.addAttribute("endDatum", hilfeleistung.getEndDatum());
@@ -241,12 +150,12 @@ public class AngebotController {
 
 		System.out.println(angebot);
 
-		// Ausleihartikel ausleihartikel = ausleihartikelService
-		// .ausleihartikelDTO_TO_Ausleihartikel(angebot);
+		Ausleihartikel ausleihartikel = ausleihartikelService
+				.ausleihartikelDTO_TO_Ausleihartikel(angebot);
 
-		// System.out.println(ausleihartikel);
+		System.out.println(ausleihartikel);
 
-		// ausleihartikelService.update(ausleihartikel);
+		ausleihartikelService.update(ausleihartikel);
 
 		return "redirect:../angebote";
 	}
