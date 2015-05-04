6ad61241416c91a3d3b53174a71fbaef62648454
Johannes G <gruen.jojo.develop@gmail.com>
2015-05-04 09:21:33 +0200

    SHARE-68: Test Coverage von 80% für Implementierung aus Sprint 1
    Task-Url: http://jira.it.dh-karlsruhe.de:8080/browse/SHARE-68

---
+++ localSharing/src/test/java/pandha/swe/localsharing/service/ServiceUnitTest.java
@@ -0,0 +1,12 @@
+package pandha.swe.localsharing.service;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ AusleiharikelServiceImplTest.class,
+		TestTauschartikelServiceImpl.class })
+public class ServiceUnitTest {
+
+}
