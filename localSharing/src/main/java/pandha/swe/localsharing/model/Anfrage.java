4dce2f08555193623a80b589176f2b35cd51d3a7
Julia Anken <jul.anken@googlemail.com>
2015-05-25 16:50:02 +0200

    SHARE-101: Model mit DAO und DTO für Anfragen senden
    Task-Url: http://jira.it.dh-karlsruhe.de:8080/browse/SHARE-101

--- localSharing/src/main/java/pandha/swe/localsharing/model/Anfrage.java
+++ localSharing/src/main/java/pandha/swe/localsharing/model/Anfrage.java
@@ -20,42 +20,38 @@ public class Anfrage {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Column(name = "anfrageID")
 	private Long anfrageid;
-	
+
 	@OneToOne
 	private Angebot angebot;
-	
+
 	@OneToOne
 	private Benutzer sender;
-	
-	@OneToOne
-	private Benutzer empfaenger;
-	
+
 	@Column(name = "DATUM")
 	private Date datum;
-	
+
 	@Column(name = "STARTDATUM")
 	private Date startDatum;
-	
+
 	@Column(name = "ENDDATUM")
 	private Date endDatum;
-	
+
 	@Column(name = "KOMMENTAR")
 	private String kommentar;
-	
+
 	@Enumerated(EnumType.STRING)
 	@Column(name = "ANFRAGESTATUS")
 	private AnfrageStatus status;
-	
+
 	@Column(name = "KONTAKT")
 	private String kontaktArt;
 
 	public Anfrage(Long anfrageid, Angebot angebot, Benutzer sender,
-			Benutzer empfaenger, Date datum, Date startDatum, Date endDatum,
-			String kommentar, AnfrageStatus status, String kontaktArt) {
+			Date datum, Date startDatum, Date endDatum, String kommentar,
+			AnfrageStatus status, String kontaktArt) {
 		this.anfrageid = anfrageid;
 		this.angebot = angebot;
 		this.sender = sender;
-		this.empfaenger = empfaenger;
 		this.datum = datum;
 		this.startDatum = startDatum;
 		this.endDatum = endDatum;
@@ -86,14 +82,6 @@ public class Anfrage {
 
 	public void setSender(Benutzer sender) {
 		this.sender = sender;
-	}
-
-	public Benutzer getEmpfaenger() {
-		return empfaenger;
-	}
-
-	public void setEmpfaenger(Benutzer empfaenger) {
-		this.empfaenger = empfaenger;
 	}
 
 	public Date getDatum() {
@@ -147,10 +135,10 @@ public class Anfrage {
 	@Override
 	public String toString() {
 		return "Anfrage [anfrageid=" + anfrageid + ", angebot=" + angebot
-				+ ", sender=" + sender + ", empfaenger=" + empfaenger
-				+ ", datum=" + datum + ", startDatum=" + startDatum
-				+ ", endDatum=" + endDatum + ", kommentar=" + kommentar
-				+ ", status=" + status + ", kontaktArt=" + kontaktArt + "]";
+				+ ", sender=" + sender + ", datum=" + datum + ", startDatum="
+				+ startDatum + ", endDatum=" + endDatum + ", kommentar="
+				+ kommentar + ", status=" + status + ", kontaktArt="
+				+ kontaktArt + "]";
 	}
-	
+
 }
